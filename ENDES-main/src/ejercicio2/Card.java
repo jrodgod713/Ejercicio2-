6b4c327868dd4ae2357f072a09167249e2e325aa
Jesús Rodríguez <jrodgod713@g.educaand.es>
2024-11-29 19:37:53 +0100

    Relación2

--- ENDES-main/src/ejercicio2/Card.java
+++ ENDES-main/src/ejercicio2/Card.java
@@ -4,13 +4,13 @@ public class Card {
 
 	public String suit;
 	public String value;
-	
+	//Creación de cartas con variables para el tipo y el número
 	public Card (String suit, String value) {
 		//
 		this.suit = suit;
 		this.value = value;
 	}
-	
+	//Uniendo el tipo y el número
 	public String toString () {
 		//
 		return (this.suit+"-"+this.value);
